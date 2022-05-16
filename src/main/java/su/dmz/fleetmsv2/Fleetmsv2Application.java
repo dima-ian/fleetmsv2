905dec38cff9cf8dbd0dfce5e07b073eee6e94ef
Dima Ian <dima.ian.80@gmail.com>
2022-05-16 16:45:37 +0300

    video 9 - done

--- src/main/java/su/dmz/fleetmsv2/Fleetmsv2Application.java
+++ src/main/java/su/dmz/fleetmsv2/Fleetmsv2Application.java
@@ -3,6 +3,8 @@ package su.dmz.fleetmsv2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//  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
+
 @SpringBootApplication
 public class Fleetmsv2Application {
 
